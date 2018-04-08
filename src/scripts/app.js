fd99ee7f7c7d2eeb94879af178cf78ccef8b6d6b
NestorPlasencia <nplasencia82@gmail.com>
2018-04-08 00:45:24 -0500

    agregar datos de .json

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,4 +2,6 @@
   main script for only runs every function
 */
 
+
+
 searchForm();
--- src/scripts/global.js
+++ src/scripts/global.js
@@ -22,9 +22,54 @@ var loadJSON = function(url, callback){
     xobj.send(null);
 };
 
-var data
+var globalData = []
+var content = document.getElementById("content")
 
-loadJSON(JSON_FILE,function(this,response) {
-    //console.log(response);
-    console.log(a);
+
+loadJSON(JSON_FILE,function(data,response) {
+    globalData = data.data;
+    renderItems()
+    listTitles()
+
+
+
 });
+
+function renderItems() {
+    
+    let elements = globalData.filter((item,ind) => ind < 9 )
+    elements.forEach( element => {
+        content.appendChild(createCard(element))
+    })
+}
+
+
+function listTitles() {
+    let titles = []
+    globalData.forEach( item => {
+       titles.push(item.title) 
+    })
+    var input = document.getElementById("search");
+    new Awesomplete(input, {
+        list: titles
+    });
+}
+
+
+function createCard(element) {
+    let card = document.createElement("DIV");
+    card.className = "card";
+    let img = document.createElement("IMG");
+    img.className = "item-img"
+    img.src = element.image;
+    let title = document.createElement("H3");
+    title.className = "item-img"
+    title.innerText  = element.title 
+    let description = document.createElement("P");
+    description.className = "item-description"
+    description.innerText  = element.teaser 
+    card.appendChild(img)
+    card.appendChild(title)
+    card.appendChild(description)
+    return card
+}
